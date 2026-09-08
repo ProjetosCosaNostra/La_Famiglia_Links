10c6c36379a596efeedc036b1abff022b6efef5f
Felipe Rosa Gomes <projetoscosanostra@gmail.com>
2026-09-08 19:46:52 -0300

    build(blackgold): bump measured calibration cache key to V19.4

--- cloudflare-beauty-finds/v19.mjs
+++ cloudflare-beauty-finds/v19.mjs
@@ -25,6 +25,6 @@ const index = path.join(out, 'index.html');
 let html = await fs.readFile(index, 'utf8');
 html = html.replace(/<link rel="stylesheet" href="\.\/blackgold-home-v19-authority\.css\?v=[^"]+"\/>/g, '');
 html = html.replace(/<link rel="stylesheet" href="\.\/blackgold-home-v19-3-calibration\.css\?v=[^"]+"\/>/g, '');
-html = html.replace('</head>', '<link rel="stylesheet" href="./blackgold-home-v19-authority.css?v=20260908-v19-2"/><link rel="stylesheet" href="./blackgold-home-v19-3-calibration.css?v=20260908-v19-3"/></head>');
+html = html.replace('</head>', '<link rel="stylesheet" href="./blackgold-home-v19-authority.css?v=20260908-v19-2"/><link rel="stylesheet" href="./blackgold-home-v19-3-calibration.css?v=20260908-v19-4"/></head>');
 await fs.writeFile(index, html, 'utf8');
-console.log(`BlackGold V19.2 authority + V19.3 calibration ready: ${expectedSize} bytes ${expectedSha256}`);
+console.log(`BlackGold V19.2 authority + V19.4 measured calibration ready: ${expectedSize} bytes ${expectedSha256}`);
