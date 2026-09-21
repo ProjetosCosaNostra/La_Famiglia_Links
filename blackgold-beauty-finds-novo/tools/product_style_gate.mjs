e04740481d51e1e5e1d39c2de8bfab71091c760d
Felipe Rosa Gomes <projetoscosanostra@gmail.com>
2026-09-21 13:28:51 -0300

    test: lock aligned showcase media height

--- blackgold-beauty-finds-novo/tools/product_style_gate.mjs
+++ blackgold-beauty-finds-novo/tools/product_style_gate.mjs
@@ -30,7 +30,7 @@ const expected={
     padding:"5px",
     mediaBg:"rgb(251, 243, 232)",
     mediaRadius:"5px",
-    mediaHeight:96.234375,
+    mediaHeight:97.234375,
     titleSize:"9px",
     titleHeight:16.015625,
     titleMarginTop:"7px",
